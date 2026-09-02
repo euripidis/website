b46a9ba002ae2f0d896c736d970fcc1eb91a7108
euripidis <97024314+euripidis@users.noreply.github.com>
2026-09-03 00:41:36 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-document.getElementById("testBtn").addEventListener("click", function() {
-  alert("Όλα δουλεύουν σωστά!");
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', event => {
+    const targetId = link.getAttribute('href');
+    const target = document.querySelector(targetId);
+    if (!target) return;
+    event.preventDefault();
+    target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  });
 });
